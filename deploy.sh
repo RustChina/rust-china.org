6b206ac0f93576d61584dc33ee485514bd26a8c6
tennix <ztennix@gmail.com>
2016-03-05 11:51:18 +0800

    use variables

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# rust-china is an alias to rust-china.org server in $HOME/.ssh/config
+server=rust-china
+remote_path=/home/tennix/rust-china.org/public
+
 echo "Generate site..."
 hexo generate
 echo "Sync site to remote machine..."
-rsync -r public/ rust-china:/home/tennix/rust-china.org/public/
+rsync -r public/ $server:$remote_path
